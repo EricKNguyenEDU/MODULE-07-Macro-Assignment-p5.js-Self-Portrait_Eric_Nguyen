682ff2b1f5e133c4ab43e7c65179ab2fa8055789
EricKNguyenEDU <eric.k.nguyen@sjsu.edu>
2025-10-23 01:44:25 -0700

    Add files via upload

---
+++ p5_js_Self_Portrait_Shapes_Draft/p5_js_Self_Portrait_Shapes_Draft.js
@@ -0,0 +1,713 @@
+function setup() {
+  textOutput();
+
+  createCanvas(1000, 1000);
+
+background(166,124,223);
+    // Start drawing the shape.
+  
+    // hair.
+  vertex(411.9237, 227.9001);
+  vertex(411.9237, 251.9787);
+  vertex(379.1277, 209.2128);
+  vertex(371.4681, 223.1143);
+  vertex(371.468, 184.0638);
+  vertex(371.4681, 164.6596);
+  vertex(380.1489, 128.3256);
+  vertex(398.5319, 103.383);
+  vertex(422.7872, 75.2979);
+  vertex(442.4468, 52.8298);
+  vertex(472.107, 32.4043);
+  vertex(492.8723, 25.766);
+  vertex(513.1702, 30.3617);
+  vertex(527.6285, 25.766);
+  vertex(565.4468, 29.3404);
+  vertex(603.2979, 43.1277);
+  vertex(619.1277, 59.4681);
+  vertex(637.9435, 82.9574);
+  vertex(650.1489, 134.5319);
+  vertex(659.9787, 210.0666);
+  vertex(650.1489, 244.3191);
+  vertex(623.3279, 281.0065);
+  vertex(630.4768, 249.3469);
+  vertex(630.4768, 249.3469);
+  vertex(624.8598, 253.432);
+  vertex(619.7534, 215.1341);
+  vertex(623.8386, 157.9427);
+  vertex(534.9875, 128.3256);
+  vertex(445.037, 193.6383);
+  
+  fill(43, 64, 102);
+
+  // Stop drawing the shape.
+  endShape(CLOSE);
+ 
+  beginShape();
+
+  // face.
+  vertex(411.9237, 295.3044);
+  vertex(411.9237, 227.9001);
+  vertex(426.2215, 198.2831);
+  vertex(451.6683, 185.5171);
+  vertex(495.6683, 156.4107);
+  vertex(534.9875, 128.3256);
+  vertex(579.413, 142.1129);
+  vertex(623.8386, 157.9427);
+  vertex(619.7534, 215.1341);
+  vertex(624.8598, 253.432);
+  vertex(630.4768, 249.3469);
+  vertex(623.3279, 281.0065);
+  vertex(608.0088, 325.9427);
+  vertex(545.7109, 386.198);
+  vertex(510.0272, 382.9159);
+  vertex(481.3705, 370.8788);
+  
+  fill(219, 156, 109);
+
+  // Stop drawing the shape.
+  endShape(CLOSE);
+
+    beginShape();
+
+  // body.
+  vertex(408.234, 386.2721);
+  vertex(583.8936, 412.3191);
+  vertex(614.5319, 433.2553);
+  vertex(689.0851, 462.8723);
+  vertex(749.3404, 500);
+  vertex(764.6596, 554.7872);
+  vertex(798.8723, 759.0426);
+  vertex(745.7681, 791.4679);
+  vertex(704.9149, 779.4681);
+  vertex(686.2219, 923.4681);
+  vertex(681.4255, 935.2128);
+  vertex(690.1064, 966.3617);
+  vertex(681.4255, 983.7234);
+  vertex(697.2553, 1000);
+  vertex(293.8511, 1000);
+  vertex(291.2979, 988.3191);
+  vertex(274.9574, 970.4468);
+  vertex(252.4894, 940.8298);
+  vertex(256.0638, 904.0638);
+  vertex(186.1064, 763.383);
+  vertex(146.2766, 743.7234);
+  vertex(149.3404, 729.6809);
+  vertex(122.7872, 706.4468);
+  vertex(166.7021, 562.9574);
+  vertex(166.7021, 539.4681);
+  vertex(208.5745, 462.8723);
+  vertex(362.7872, 401.5957);
+  vertex(391.6959, 386.2721);
+  
+  fill(230, 230, 230);
+
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+      beginShape();
+
+  // neck.
+  vertex(411.9237, 295.3044);
+  vertex(481.3705, 370.8788);
+  vertex(510.0272, 382.9159);
+  vertex(545.7109, 386.198);
+  vertex(608.0088, 325.9427);
+  vertex(587.9787, 386.2721);
+  vertex(583.8936, 412.3191);
+  vertex(569.5957, 466.9574);
+  vertex(553.766, 497.5957);
+  vertex(519.0426, 502.1915);
+  vertex(466.9574, 483.2979);
+  vertex(408.234, 426.1064);
+  
+  fill(166, 124, 82);
+
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+        beginShape();
+
+  // ear.
+  vertex(379.1277, 209.2128);
+  vertex(411.9237, 251.9787);
+  vertex(411.9237, 295.3044);
+  vertex(399.0426, 295.8936);
+  vertex(391.6959, 282.1064);
+  vertex(386.2766, 276.4894);
+  vertex(380.1489, 262.7021);
+  vertex(371.4681, 223.1143);
+  
+  fill(240, 156, 109);
+
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+          beginShape();
+
+  // ear detail 1.
+  vertex(379.6383, 223.1143);
+  vertex(389.3404, 231.0426);
+  vertex(375.5532, 231.0426);
+  
+  fill(166, 124, 82);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE);
+  
+           beginShape();
+
+  // ear detail 2.
+  vertex(391.6959, 239.2128);
+  vertex(402.617, 252.5532);
+  vertex(402.617, 259.1277);
+  vertex(405.6809, 268.8298);
+  vertex(396.234, 263.2128);
+  vertex(391.6959, 266.2481);
+  vertex(384.7447, 262.7021);
+  vertex(386.7872, 242.2766);
+  
+  fill(166, 124, 82);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+    // shirt detail 1.
+      beginShape();
+  vertex(749.3404, 500);
+  vertex(841.2553, 722.8071);
+  vertex(796.5703, 745.2988);
+  
+  fill(179, 179, 179);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+      // shirt detail 2.
+      
+        beginShape();
+  vertex(269.8511, 639.5532);
+  vertex(274.9574, 721.766);
+  vertex(287.2128, 729.6809);
+  vertex(287.2128, 748.8298);
+  vertex(257.2416, 763.6383);
+  vertex(268.8298, 793.766);
+  vertex(237.1702, 785.5957);
+  vertex(224.9149, 763.6383);
+  vertex(252.4894, 748.8298);
+  vertex(261.6809, 748.8298);
+  
+  fill(179, 179, 179);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+                 // bag.
+      
+        beginShape();
+  vertex(237.1702, 875.4681);
+  vertex(320, 859);
+  vertex(400, 907 );
+  vertex(410, 1000);
+  vertex(293.8511, 1000);
+  vertex(291.2979, 988.3191);
+  vertex(276, 972);
+  vertex(267.2979, 956.1489);
+  vertex(252, 943);
+  vertex(256.0638, 904.0638);
+  
+  fill(0, 0, 0);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+       // left arm detail 1.
+      
+        beginShape();
+  vertex(122.7872, 706.4468);
+  vertex(149.3404, 729.6809);
+  vertex(146.2766, 743.7234);
+  vertex(186.1064, 763.383);
+  vertex(122.7872, 780.4894);
+  vertex(117.6809, 821.8511);
+  vertex(112.5745, 776.4043);
+  
+  fill(166, 89, 82);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+         // left arm.
+      
+        beginShape();
+  vertex(186.1064, 763.383);
+  vertex(224.9149, 763.6383);
+  vertex(237.1702, 785.5957);
+  vertex(296.4043, 802.9574);
+  vertex(272.9149, 842.7872);
+  vertex(272.9149, 875.4681);
+  vertex(237.1702, 875.4681);
+  vertex(138.1064, 841.766);
+  vertex(117.6809, 821.8511);
+  vertex(122.7872, 780.4894);
+  
+  fill(219, 156, 109);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+           // wrist watch.
+      
+        beginShape();
+  vertex(296.4043, 802.9574);
+  vertex(326.0213, 802.9574);
+  vertex(308.2973, 826.6542);
+  vertex(296.4043, 846.8723);
+  vertex(296.4043, 880.5745);
+  vertex(272.9149, 875.4681);
+  vertex(272.9149, 842.7872);
+  
+  fill(43, 64, 102);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+             // wrist watch detail.
+      
+        beginShape();
+  vertex(326.0213, 802.9574);
+  vertex(335.2128, 819.4255);
+  vertex(321.755, 819.4255);
+  vertex(308.2973, 826.6542);
+  vertex(316.8298, 812.1489);
+  
+  fill(23, 29, 35);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+         // wrist.
+      
+        beginShape();
+  vertex(333.708, 816.7295);
+  vertex(364.8298, 821.8511);
+  vertex(335.2128, 852.4894);
+  vertex(326.0213, 880.5745);
+  vertex(296.4043, 875.4681);
+  vertex(296.4043, 846.8723);
+  vertex(308.2973, 826.6542);
+  vertex(321.755, 819.4255);
+  
+  fill(166, 89, 82);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+           // left hand 1.
+      
+        beginShape();
+  vertex(364.8298, 821.8511);
+  vertex(378.1064, 821.8511);
+  vertex(408.234, 831.9362);
+  vertex(408.234, 890.7872);
+  vertex(341.8511, 904.0638);
+  vertex(335.2128, 888.234);
+  vertex(326.0213, 880.5745);
+  vertex(335.2128, 852.4894);
+  vertex(355.1277, 826.6542);
+  
+  fill(219, 156, 109);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+               // left hand 2.
+      
+        beginShape();
+  vertex(341.8511, 904.0638);
+  vertex(360.7447, 911.2128);
+  vertex(384.234, 921.9362);
+  vertex(449.5957, 962.2766);
+  vertex(455.7234, 957.1702);
+  vertex(451.6383, 932.1489);
+  vertex(517.5106, 932.1489);
+  vertex(521.3218, 928.9837);
+  vertex(536.1489, 935.2128);
+  vertex(503.2128, 957.1702);
+  vertex(459.8085, 966.3617);
+  vertex(392.4043, 953.5957);
+  vertex(381.1702, 941.3404);
+  vertex(341.8511, 907.1277);
+  
+  fill(166, 89, 82);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+             // left hand thumb.
+      
+        beginShape();
+  vertex(382.1915, 875.4681);
+  vertex(396.234, 877.5106);
+  vertex(451.6383, 932.1489);
+  vertex(455.7234, 957.1702);
+  vertex(449.5957, 962.2766);
+  vertex(384.234, 921.9362);
+  vertex(360.7447, 911.2128);
+  
+  fill(219, 156, 109);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+               // left middle finger.
+      
+        beginShape();
+  vertex(547.6383, 907.1277);
+  vertex(562.8881, 907.1277);
+  vertex(574.7021, 918.8723);
+  vertex(544.4357, 935.2128);
+  vertex(536.1489, 935,2128);
+  vertex(521.3218, 928.9837);
+  
+  fill(219, 156, 109);
+  
+    // Stop drawing the shape.
+  
+  endShape(CLOSE); 
+  
+                 // left pinky.
+      
+        beginShape();
+  vertex(553.766, 886.1915);
+  vertex(565.5106, 886.1915);
+  vertex(574.7021, 896.9149);
+  vertex(562.8881, 907.1277);
+  vertex(547.6383, 907.1277);
+  
+  fill(166, 89, 82);
+  
+    // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+                   // bottle.
+      
+        beginShape();
+  vertex(426.8511, 733.3404);
+  vertex(531.5319, 732.5745);
+  vertex(539.7021, 741);
+  vertex(554, 764.4894);
+  vertex(554, 887.0426);
+  vertex(547.8723, 907.9787);
+  vertex(517.7447, 933);
+  vertex(451.8723, 933);
+  vertex(408.4681, 891.6383);
+  vertex(408.4681, 759.9936);
+  vertex(418.1702, 742.0213);
+  
+  fill(135, 163, 214);
+  
+      // Stop drawing the shape.
+  endShape(CLOSE); 
+
+          beginShape();
+
+  // bag sash.
+  vertex(583.8936, 412.3191);
+  vertex(536.1489, 695);
+  vertex(495.0426, 675.4452);
+  
+  fill(0, 0, 0);
+
+  // Stop drawing the shape.
+  endShape(CLOSE);
+                    // bottle 2.
+      
+        beginShape();
+  vertex(426.617, 707.9787);
+  vertex(531.2979, 703.8936);
+  vertex(531.2979, 731.7234);
+  vertex(521.3218, 748.8298);
+  vertex(500, 755.4681);
+  vertex(459.8085, 755.4681);
+  vertex(432.234, 746.2766);
+  vertex(426.617, 732.4894);
+  
+  fill(43, 64, 102);
+  
+      // Stop drawing the shape.
+  endShape(CLOSE); 
+
+                     // bottle 3.
+      
+        beginShape();
+  vertex(448, 664);
+  vertex(500, 664.0638);
+  vertex(528.7447, 669.6809);
+  vertex(531.2979, 703.8936);
+  vertex(525.6809, 710.5319);
+  vertex(507.8085, 718.7021);
+  vertex(461.8511, 721.766);
+  vertex(437.0851, 716.1489);
+  vertex(426.617, 707.9787);
+  vertex(426.617, 700.8298);
+  vertex(436.3191, 678.3617);
+  
+  fill(139, 64, 102);
+  
+      // Stop drawing the shape.
+  endShape(CLOSE); 
+  
+                       // right arm detail.
+      
+        beginShape();
+  vertex(798.8723, 759.0426);
+  vertex(812.1489, 812.1489);
+  vertex(777.9362, 783.0426);
+  vertex(745.7681, 781.4679);
+  
+  fill(166, 124, 82);
+  
+      // Stop drawing the shape.
+  endShape(CLOSE);
+  
+                         // right arm.
+      
+        beginShape();
+  vertex(639.5532, 748.8298);
+  vertex(704.9149, 779.4681);
+  vertex(777.9362, 783.0426);
+  vertex(812.1489, 812.1489);
+  vertex(801.9362, 848.4043);
+  vertex(799.383, 875.4681);
+  vertex(759.5532, 894.8723);
+  vertex(743.7234, 886.7021);
+  vertex(697.766, 878.0213);
+  vertex(672.234, 864.7447);
+  vertex(612.4994, 808.0638);
+  
+  fill(219, 156, 109);
+  
+      // Stop drawing the shape.
+  endShape(CLOSE);
+  
+                           // right hand.
+      
+        beginShape();
+  vertex(612.4894, 718.7021);
+  vertex(626.2866, 740.6596);
+  vertex(639.5532, 748.8298);
+  vertex(612.4894, 808.0638);
+  vertex(583.8936, 797.3404);
+  vertex(572.6596, 765.6809);
+  vertex(565.5106, 729.9362);
+  
+  fill(219, 156, 109);
+  
+      // Stop drawing the shape.
+  endShape(CLOSE);
+  
+                             // right pinky.
+      
+        beginShape();
+  vertex(501, 664.0638);
+  vertex(525.9059, 668.9307);
+  vertex(539.9574, 669.6809);
+  vertex(571.617, 671.7234);
+  vertex(593.0638, 697.766);
+  vertex(615.5319, 739.1277);
+  vertex(582.3404, 748.8298);
+  vertex(554.766, 700.3191);
+  vertex(540.9787, 695.0957);
+  vertex(532.2979, 695.2128);
+  vertex(505.7447, 679.8936);
+  
+  fill(219, 156, 109);
+  
+      // Stop drawing the shape.
+  endShape(CLOSE);
+  
+                               // right middle finger.
+      
+        beginShape();
+  vertex(426.617, 644.1489);
+  vertex(460.3191, 639.5532);
+  vertex(500, 643);
+  vertex(547.6383, 671.7234);
+  vertex(500, 664.0638);
+  vertex(443.4681, 664.0638);
+  vertex(431.2128, 659.4681);
+  
+  fill(166, 89, 82);
+  
+      // Stop drawing the shape.
+  endShape(CLOSE);
+  
+        beginShape();
+
+  // right hand detail2.
+  vertex(554.766, 700.3191);
+  vertex(579.1662, 723.6096);
+  vertex(582.3404, 748.8298);
+  vertex(572.2189, 743.5729);
+  
+  fill(166, 124, 82);
+
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+            beginShape();
+
+  // shirt detail 2.
+  vertex(408.234, 426.1064);
+  vertex(408.234, 386.2721);
+  vertex(391.6959, 386.2721);
+  
+  fill(179, 179, 179);
+ // Stop drawing the shape.
+  endShape(CLOSE);
+            beginShape();
+
+  // shirt detail 3.
+  vertex(583.8936, 412.3191);
+  vertex(614.5319, 433.2553);
+  vertex(567, 498);
+  
+  fill(179, 179, 179);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+              beginShape();
+
+  // lowerlip.
+  vertex(492.8723, 311.7234);
+  vertex(577.1277, 321.9362);
+  vertex(545.4864, 341.8511);
+  vertex(515.3232, 337.2553);
+  
+  fill(166, 89, 82);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+                beginShape();
+
+  // upperlip.
+  vertex(535, 295.8936);
+  vertex(545.4864, 302.0213);
+  vertex(554.2766, 299.9787);
+  vertex(577.1277, 321.9362);
+  vertex(551.7234, 316.8298);
+  vertex(539.9787, 321.9362);
+  vertex(525.1702, 315.2979);
+  vertex(492.8723, 311.7234);
+  
+  fill(166, 124, 82);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+                  beginShape();
+
+  // nose.
+  vertex(539, 234);
+  vertex(552,276);
+  vertex(539, 267);
+  
+  fill(166, 124, 82);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+                    beginShape();
+
+  // left eye ball.
+  vertex(487, 198);
+  vertex(498, 199);
+  vertex(498, 208);
+  vertex(487, 208);
+  
+  fill(0, 0, 0);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+                      beginShape();
+
+  // left eye lid.
+  vertex(461.0851, 196.3191);
+  vertex(490, 191);
+  vertex(499, 192);
+  vertex(513.1702, 201.4255);
+  
+  fill(166, 124, 82);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+                      beginShape();
+
+  // right eye ball.
+  vertex(586.5, 211);
+  vertex(597.5, 212);
+  vertex(597.5, 221);
+  vertex(586.5, 221);
+  
+  fill(0, 0, 0);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+                      beginShape();
+
+  // right eye lid.
+  vertex(592.9149, 205);
+  vertex(601.9149, 206);
+  vertex(616.0851, 215.4255);
+  vertex(564, 210.3191);
+  
+  fill(166, 124, 82);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+  beginShape();
+
+  // left eyebrow.
+  vertex(495.6683, 156.4107);
+  vertex(510.8723, 160.0638);
+  vertex(531.2979, 181.1915);
+  vertex(472.107, 171.8475);
+  
+  fill(43, 64, 102);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+    beginShape();
+
+  // right eyebrow.
+  vertex(577.1277, 168.7447);
+  vertex(610.9574, 173.3404);
+  vertex(621.2889, 193.6383);
+  vertex(610.9574, 184.0638);
+  vertex(560.3264, 184.0638);
+  vertex(567, 174);
+  
+  fill(43, 64, 102);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  noFill();
+  circle(490, 220, 77.617, 77.617);
+  noFill();
+  circle(600, 220, 77.617, 77.617);
+  
+    beginShape();
+
+  // glasses middle.
+  vertex(527, 209);
+  vertex(563, 214);
+  vertex(561, 218);
+  vertex(528, 214);
+  
+  fill(0, 0, 0);
+  // Stop drawing the shape.
+  endShape(CLOSE);
+  
+}
+
+
+function draw() {
+
+}
